24d41d31371996a0a54bd3456718c137653ba487
einaralex <einaralex@gmail.com>
2024-06-12 23:38:18 +0000

    testing4

--- apps/customer/app/page.tsx
+++ apps/customer/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 import { MoneriumConnect } from 'components/MoneriumConnect/MoneriumConnect';
 
