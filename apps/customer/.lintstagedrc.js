ea7f1446226897bf114c4b8d0f976a4974232efb
einaralex <einaralex@gmail.com>
2024-06-13 00:23:28 +0000

    fix

--- apps/customer/.lintstagedrc.js
+++ apps/customer/.lintstagedrc.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')} &&`;
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')} && prettier --write`;
 
 module.exports = {
-  '*.{ts,tsx}': [buildEslintCommand, 'prettier --write'],
+  '*.{ts,tsx}': [buildEslintCommand],
   '*.{js, json,md}': ['prettier --write'],
   '*.{css,scss,sass}': ['pnpm lint:style'],
 };
